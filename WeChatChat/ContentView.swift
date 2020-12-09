52126d19f46399a6544d2d0cb5d83b5006c880da
rynxiao <yuzhongzi91@sina.com>
2020-12-09 23:02:21 +0800

    Initial Commit

---
+++ WeChatChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeChatChat
+//
+//  Created by Ryn on 2020/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeChatChat/WeChatChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChatChatApp.swift
+//  WeChatChat
+//
+//  Created by Ryn on 2020/12/9.
+//
+
+import SwiftUI
+
+@main
+struct WeChatChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
